042392d5734df66a11abe5d5ba2f2b5c8dce8924
eamiear <justcall_me_k@163.com>
2020-06-18 18:18:00 +0800

    昂宝中台 客户接口、套餐接口

---
+++ src/api/service.js
@@ -0,0 +1,34 @@
+import { getAction, postAction, putAction, deleteAction } from '@/utils/ajax'
+
+const _PREFIX = '/accp'
+
+////// 客户模块
+
+// 获取客户列表
+const getConsumerList = (params) => getAction(_PREFIX + '/mp/getConsumerList', params)
+
+// 获取客户详情信息
+const getConsumerDetail = (id) => getAction(_PREFIX + '/mp/getConsumerDetail', {id})
+
+// 编辑客户信息
+const editConsumerList = (params) => postAction(_PREFIX + '/mp/editConsumerInfo', params)
+
+// 删除客户信息
+const delConsumerList = (params) => deleteAction(_PREFIX + '/mp/delConsumerDetail', params)
+
+////// 套餐模块
+
+// 获取套餐列表
+const getPackageList = (params) => getAction(_PREFIX + '/mp/getPackageList', params)
+
+// 获取套餐详情
+const getPackageDetail = (params) => getAction(_PREFIX + '/mp/getPackageDetail', params)
+
+// 新增套餐
+const addPackage = (params) => postAction(_PREFIX + '/mp/addPackage', params)
+
+// 编辑套餐
+const editPackageDetail = (params) => postAction(_PREFIX + '/mp/editPackageDetail', params)
+
+// 删除套餐
+const delPackage = (params) => deleteAction(_PREFIX + '/mp/delPackage', params)
--- vue.config.js
+++ vue.config.js
@@ -61,6 +61,14 @@ module.exports = {
           '/pro': ''  //默认所有请求都加了/pro前缀，需要去掉
         }
       },
+      '/accp': {
+        target: 'https://aliiot.on-bright.com/mock/37/accp', //mock API接口系统
+        ws: false,
+        changeOrigin: true,
+        pathRewrite: {
+          '/accp': ''  //默认所有请求都加了/pro前缀，需要去掉
+        }
+      },
       '/nurse': {
         target: 'http://192.168.200.241/nurse',
         // target: 'https://aliiot.on-bright.com/nurse',
